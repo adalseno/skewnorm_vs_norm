33e5eb9b2671c2fdaa06f53432a7a3a25bbfcab0
Andrea Dalseno <adalseno@yahoo.com>
2023-07-25 01:49:37 +0200

    Update skewnorm_vs_norm.py

--- skewnorm_vs_norm.py
+++ skewnorm_vs_norm.py
@@ -10,7 +10,7 @@ col1, col2 = st.columns(2)
 with col1:
     # Input area
     st.header("Select parameters for the distribution")
-    alpha = st.number_input('alpha')
+    alpha = st.number_input('alpha', value=1)
     mu = st.number_input('mu')
     sigma = st.number_input('sigma', value=1)
 
@@ -18,7 +18,7 @@ with col2:
     # plot area
     st.header("PDF of skew-normal vs normal")
     fig, ax = plt.subplots(1, 1)
-    x= np.linspace(ss.skewnorm.ppf(0.01, alpha),ss.skewnorm.ppf(0.99, a), 100)
+    x= np.linspace(ss.skewnorm.ppf(0.01, alpha),ss.skewnorm.ppf(0.99, alpha), 100)
     ax.plot(x, ss.skewnorm.pdf(x, alpha, mu, sigma),'r-', lw=5, alpha=0.6, label='skewnorm')
     ax.plot(x, ss.norm.pdf(x, mu, sigma),'k-', lw=5, alpha=0.6, label='norm')
     st.pyplot(fig)
